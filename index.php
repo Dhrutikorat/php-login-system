ce73a786ebd55407ae778eaa54e3962d0570d769
dhruti.korat.sos <dhruti.korat@shivohm.com>
2021-08-25 05:01:49 -0500

    adding login template

--- index.php
+++ index.php
@@ -1,2 +1,44 @@
-<?php
-echo "hello world";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="Absolute-Center is-Responsive">
+                <div id="logo-container"></div>
+                <div class="col-sm-12 col-md-10 col-md-offset-1">
+                    <h2 class="text-center m-4">Login</h2>
+                    <form  id="loginForm" class="js-login">
+                        <div class="form-group input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input class="form-control" type="text" name='username' placeholder="username" required='required'/>
+                        </div>
+                        <div class="form-group input-group mt-2">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input class="form-control" type="password" name='password' placeholder="password" required='required'/>
+                        </div>
+                        <div class="form-group mt-4 ">
+                            <button type="submit" class="btn btn-primary btn-sm" style="float:right;">Login</button>
+                        </div>
+                        <div class="form-group text-center">
+                            <a href="#">Forgot Password</a>&nbsp;|&nbsp;<a href="#">Sign Up</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+
+</html>
